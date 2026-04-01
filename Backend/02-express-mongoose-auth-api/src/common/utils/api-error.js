a9fb6ebe8cd8eb3f229a21b7f4e534d906693d6c
Dnyaneshwar More <dnyaneshwarmore2500@gmail.com>
2026-04-02 00:22:58 +0530

    feat: implement registration functionality with user service and routes

--- Backend/02-express-mongoose-auth-api/src/common/utils/api-error.js
+++ Backend/02-express-mongoose-auth-api/src/common/utils/api-error.js
@@ -14,6 +14,10 @@ class ApiError extends Error {
     static unauthorized(message = "unauthorized") {
         return new ApiError(401, message)
     }
+
+    static conflict(message = "Conflict") {
+        return new ApiError(409, message)
+    }
 }
 
 export default ApiError;
---
+++ Backend/02-express-mongoose-auth-api/src/modules/auth/auth.service.js
@@ -0,0 +1,11 @@
+import ApiError from "../../common/utils/api-error.js"
+import User from "./auth.model.js"
+
+const register = async ({name, email, password, role}) => {
+    
+    const existing = await User.findOne({email})
+    if(existing) throw ApiError.conflict("Email already exists")
+    return userObj
+}
+
+export {register}
