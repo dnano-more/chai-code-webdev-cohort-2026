6ac76c024e94a661529adf20795ae1fa36a0c396
Dnyaneshwar More <dnyaneshwarmore2500@gmail.com>
2026-04-02 12:23:45 +0530

    feat: add JWT utility functions and integrate token generation in registration and login processes

--- Backend/02-express-mongoose-auth-api/src/modules/auth/auth.service.js
+++ Backend/02-express-mongoose-auth-api/src/modules/auth/auth.service.js
@@ -1,11 +1,59 @@
 import ApiError from "../../common/utils/api-error.js"
+import { generateAccessToken, generateRefreshToken, generateResetToken } from "../../common/utils/jwt.utils.js"
 import User from "./auth.model.js"
+
+const hashToken = (token) => crypto.createHash("sha256").update(token).digest("hex");
 
 const register = async ({name, email, password, role}) => {
     
     const existing = await User.findOne({email})
     if(existing) throw ApiError.conflict("Email already exists")
+    
+    const {rawToken, hashedToken} = generateResetToken()
+
+    const user = await User.create({
+        name: name,
+        email: email,
+        password: password,
+        role: role,
+        varificationToken: hashedToken
+    })
+
+    //TODO: send an email to user with token: rawToken
+
+
+    const userObj = user.toObject()
+    delete userObj.password
+    delete userObj.varificationToken
+        
     return userObj
+}
+
+const login = async ({email, password}) => {
+    // take email and find user in DB
+    // then check if password is correct
+    // check if verified or not
+
+    const user = await User.findOne({email}).select("+password")
+    if(!user) throw ApiError.unauthorized("Invalid Email or Password")
+
+    // somehow I will check password
+
+    if(!user.isVerified) {
+        throw ApiError.forbidden("Please verify your email before login")
+    }
+
+    const accessToken = generateAccessToken({id: user._id, role: user.role})
+    const refreshToken = generateRefreshToken({id: user._id})
+
+    user.refreshToken = hashToken(refreshToken)
+    await user.save({validateBeforeSave: false})
+
+    const userObj = user.toObject()
+    delete userObj.password
+    delete userObj.refreshToken
+
+    return {user: userObj, accessToken, refreshToken}
 }
 
 export {register}
