6f8d2663cda03d84b940a985554e5d19a5bbd865
Dnyaneshwar More <dnyaneshwarmore2500@gmail.com>
2026-04-03 12:34:05 +0530

    feat: implement user authentication with bcrypt, add password hashing and comparison methods

--- Backend/02-express-mongoose-auth-api/src/modules/auth/auth.model.js
+++ Backend/02-express-mongoose-auth-api/src/modules/auth/auth.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -35,5 +36,16 @@ const userSchema = new mongoose.Schema({
     resetPasswordToken: {type: String, select: false},
     resetPasswordExpires: {type: Date, select: false}
 }, {timestamps: true});
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified("password")) return next();
+    
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+})
+
+userSchema.methods.comparePassword = async function(clearTextPassword) {
+    return bcrypt.compare(clearTextPassword, this.password);
+}
 
 export default mongoose.model("User", userSchema)
